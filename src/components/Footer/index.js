41f01ba7e06ced73ac7ecb778c6bb15d639618bd
Eugene Monnier <eugene.monnier@gmail.com>
2020-07-02 00:30:00 -0700

    Completed React Things Creator

---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer (props) {
+  return (
+    <footer>
+      <h3>{props.trademark}</h3>
+    </footer>
+  )
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Header (props) {
+  return <h1>I have {props.things.length} different things in my room.</h1>
+}
